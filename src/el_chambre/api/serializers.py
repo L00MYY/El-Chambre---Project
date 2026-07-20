aaf74f74ce5b0a48ee00f5cad1e6cdea8181dc0d
eandress <20255816@esen.edu.sv>
2026-07-20 11:47:00 -0600

    crear esqueleto de serializadores de la API

--- src/el_chambre/api/serializers.py
+++ src/el_chambre/api/serializers.py
@@ -1,1 +1,30 @@
+def serialize_sucursal(sucursal):
+    ...
 
+
+def serialize_producto(producto):
+    ...
+
+
+def serialize_materia_prima(materia_prima):
+    ...
+
+
+def serialize_inventario_producto(inventario):
+    ...
+
+
+def serialize_inventario_materia_prima(inventario):
+    ...
+
+
+def serialize_receta(detalles):
+    ...
+
+
+def serialize_venta(venta):
+    ...
+
+
+def serialize_produccion(produccion):
+    ...
