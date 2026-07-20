a98e0a8db4f9f3c4b2458af4644130a2114ea450
eandress <20255816@esen.edu.sv>
2026-07-20 09:18:00 -0600

    crear esqueleto de dependencias de la API

--- src/el_chambre/api/dependencies.py
+++ src/el_chambre/api/dependencies.py
@@ -1,1 +1,18 @@
+def get_catalogo_service():
+    ...
 
+
+def get_inventario_service():
+    ...
+
+
+def get_receta_service():
+    ...
+
+
+def get_venta_service():
+    ...
+
+
+def get_produccion_service():
+    ...
