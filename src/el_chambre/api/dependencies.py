ba99c82f50ce37011a8430eec9d4cc4b8d2ee95d
eandress <20255816@esen.edu.sv>
2026-07-21 14:13:00 -0600

    implementar construcción de application services

--- src/el_chambre/api/dependencies.py
+++ src/el_chambre/api/dependencies.py
@@ -1,18 +1,26 @@
+from el_chambre.application.services.catalogo_service import CatalogoService
+from el_chambre.application.services.inventario_service import InventarioService
+from el_chambre.application.services.produccion_service import ProduccionService
+from el_chambre.application.services.receta_service import RecetaService
+from el_chambre.application.services.venta_service import VentaService
+from el_chambre.infrastructure.unit_of_work import SqliteUnitOfWork
+
+
 def get_catalogo_service():
-    ...
+    return CatalogoService(SqliteUnitOfWork)
 
 
 def get_inventario_service():
-    ...
+    return InventarioService(SqliteUnitOfWork)
 
 
 def get_receta_service():
-    ...
+    return RecetaService(SqliteUnitOfWork)
 
 
 def get_venta_service():
-    ...
+    return VentaService(SqliteUnitOfWork)
 
 
 def get_produccion_service():
-    ...
+    return ProduccionService(SqliteUnitOfWork)
